1bb6671fa1b3a15f316304c9362aa8cfd6062a78
yagosenhorini <yagosenhorini30@gmail.com>
2018-03-01 19:04:08 -0300

    Implement assert to access index route

--- tests/unit/contact/route-test.js
+++ tests/unit/contact/route-test.js
@@ -1,11 +1,19 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { visit, click, currentURL } from '@ember/test-helpers'
 
-module('Unit | Route | contact', function(hooks) {
+module('Unit | Route | contact', function (hooks) {
   setupTest(hooks);
 
-  test('Verificação de exitência de rota de contato', function(assert) {
+  test('Verificação de exitência de rota de contato', function (assert) {
     let route = this.owner.lookup('route:contact');
     assert.ok(route, 'Rota de contato existente');
   });
+
+  test('Deve visitar a rota index', function (assert){
+    click('[data-test-index]');
+    andThen(()=>{
+      assert.equal(currentURL('/'), 'Rota index visitada');
+    })
+  })
 });

a8b66716cc1f61b4a7655ce963aabf3693df98f6
yagosenhorini <yagosenhorini30@gmail.com>
2018-03-02 21:18:38 -0300

    Implement new assertion to login failed

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -2,29 +2,29 @@ import { module, test } from 'qunit';
 import { visit, currentURL, click, fillIn } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
-module('Acceptance | login', function(hooks) {
+module('Acceptance | login', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /login', async function(assert) {
+  test('visiting /login', async function (assert) {
     await visit('/login');
     assert.equal(currentURL(), '/login');
   });
 
-  test('Deve efetuar o login', async function(assert){
+  test('Deve efetuar o login', async function (assert) {
     await visit('/login');
     await fillIn(('[data-test-identification]'), 'exemplo@exemplo.com');
     await fillIn(('[data-test-password]'), 'mudar1234');
-    await click('[data-test-loginButton]').then(()=>{
+    await click('[data-test-loginButton]').then(() => {
       assert.equal(currentURL(), '/home', 'Login efetuado com sucesso');
     });
   });
 
-  test('Não deve efetuar o login corretamente', async assert=>{
+  test('Não deve efetuar o login corretamente', async assert => {
     await visit('/login');
     await fillIn(('[data-test-identification]'), 'exempro@ezempro.coum');
     await fillIn(('[data-test-password]'), 'nao sei a senha');
-    await click('[data-test-loginButton]').then(()=>{
-      assert.deepEqual('exemplo@exemplo.com', 'Usuário inválido');
+    await click('[data-test-loginButton]').then(() => {
+      assert.equal(this.element('[data-test-identification').textContent, 'exemplo@exemplo.com');
     });
   });
 

db37a3ab7949a0de0007ae4d3ab69160dd64f585
AleBatata <batata.ale@gmail.com>
2018-03-13 20:54:40 -0300

    Adjust on CSS sign page

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,18 +1,19 @@
 import Ember from 'ember';
 import Route from '@ember/routing/route';
+import {inject as service} from '@ember/service';
 
 export default Route.extend({
-  fb: Ember.inject.service(),
+  fb:service(),
 
   beforeModel() {
-    return this.get('fb').getLoginStatus().then((response) => {
-      if (response.status === 'connected') {
-        let fbToken = response.authResponse.accessToken;
-        this.get('fb').setAccessToken(fbToken);
-        this.transitionTo('home');
-      } else {
-        this.transitionTo('login');
-      }
-    });
+    // return this.get('fb').getLoginStatus().then((response) => {
+    //   if (response.status === 'connected') {
+    //     let fbToken = response.authResponse.accessToken;
+    //     this.get('fb').setAccessToken(fbToken);
+    //     this.transitionTo('home');
+    //   } else {
+    //     this.transitionTo('login');
+    //   }
+    // });
   }
 });

cb70f1d852bd405597964632b6e6f9cdaf40d596
yagosenhorini <yagosenhorini30@gmail.com>
2018-03-14 21:53:44 -0300

    Change images names and adjust layout

--- app/home/route.js
+++ app/home/route.js
@@ -1,8 +1,9 @@
 import Route from '@ember/routing/route';
-import {inject as service} from '@ember/service';
+import { inject as service } from '@ember/service';
 export default Route.extend({
-    fb:service(),
-    model(){
+    fb: service(),
+    model() {
+        console.log(this.get('fb').api('/me'))
         return this.get('fb').api('/me');
     }
 });

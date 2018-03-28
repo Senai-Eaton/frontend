eb9decb7f6de1b7af6c41e69798952ed23b5ab90
yagosenhorini <yagosenhorini30@gmail.com>
2018-03-28 19:11:08 -0300

    Insert model for facebook login

--- app/index/route.js
+++ app/index/route.js
@@ -1,5 +1,10 @@
 import Route from '@ember/routing/route';
-
+import {inject as service} from '@ember/service';
 export default Route.extend({
+    fb: service(),
+    
+    model(){
+        return this.get('fb').api('/me');
+    }
 
 });

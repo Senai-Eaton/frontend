afe8b4e8d594dffd8b990af432d765bfe22b2f2a
yagosenhorini <yagosenhorini30@gmail.com>
2018-03-29 20:41:58 -0300

    Comment models

--- app/home/route.js
+++ app/home/route.js
@@ -5,9 +5,9 @@ export default Route.extend({
     fb: service(),
     model() {
         return Ember.RSVP.hash({
-            user: this.get('store').findRecord('user', 1),
+            // user: this.get('store').findRecord('user', 1),
             fb: this.get('fb').api('/me'),
-            atuacao: this.get('store').findRecord('atuacao', 2)
+            // atuacao: this.get('store').findRecord('atuacao', 2)
         });
     },
   actions:{

8111cea10f954f2de0a1b974316e15b6e316e63e
yagosenhorini <yagosenhorini30@gmail.com>
2018-04-02 20:42:26 -0300

    Refactor code

--- app/signin/route.js
+++ app/signin/route.js
@@ -1,10 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    model(){
-        return {
-            endereco: this.get('store').createRecord('endereco'),
-            voluntario: this.get('store').createRecord('voluntario')
-        };
-    }
+    // model(){
+    //    return this.get('store').findAll('atuacao');
+    // }
 });

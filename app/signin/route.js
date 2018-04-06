262d4083e38a4c88f0cb62b45e43d30134ed0150
yagosenhorini <yagosenhorini30@gmail.com>
2018-04-06 13:33:03 -0300

    Try to implements jwt

--- app/signin/route.js
+++ app/signin/route.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(){
-        return this.get('store').createRecord('cadastrarvoluntario');
+        return this.get('store').createRecord('usuario');
     }
 });

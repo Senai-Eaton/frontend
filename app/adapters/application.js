e873c3e109fdf05454806234927c593911fa5c7b
yagosenhorini <yagosenhorini30@gmail.com>
2018-04-06 13:31:52 -0300

    Adjust url templates

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
+
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.RESTAdapter.extend(DataAdapterMixin, {
-    authorizer: 'authorizer:jwt'
-});
+export default DS.RESTAdapter.extend(DataAdapterMixin, { namespace: 'api', authorizer: 'authorizer:token', headers: { withCredentials: true } });
--- app/adapters/usuario.js
+++ app/adapters/usuario.js
@@ -5,7 +5,7 @@ export default DS.RESTAdapter.extend(urlTemplates,{
     host: 'http://corujasdev-001-site4.etempurl.com',
     namespace: 'api',
     // findAllUrlTemplate: '{+host}/api/usuario/listar',
-    queryRecordUrlTemplate: '{+host}/api/usuario/buscaporid{/id}',
-    createRecordUrlTemplate: '{+host}/api/usuario/cadastrarempresa',
+    findRecordUrlTemplate: '{+host}/api/usuario/buscaporid{/id}',
+    // createRecordUrlTemplate: '{+host}/api/usuario/cadastrarempresa',
     createRecordUrlTemplate: '{+host}/api/usuario/cadastrarvoluntario',
 });

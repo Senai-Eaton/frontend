06a2db83fbb3d1ce3bf006b5aa4451840a0e7846
yagosenhorini <yagosenhorini30@gmail.com>
2018-04-06 13:27:49 -0300

    implements model routes on home page

--- app/home/route.js
+++ app/home/route.js
@@ -5,12 +5,13 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
     fb: service(),
     model(params) {
+        // console.log(this.get('store').findAll('evento'))
         return Ember.RSVP.hash({
-            // user: this.get('store').findRecord('user', 1),
             fb: this.get('fb').api('/me'),
-            // empresa: this.get('store').findRecord('empresa', 1),
+            endereco: this.get('store').findAll('endereco'),
             empresa: this.get('store').findAll('empresa'),
-            // evento: this.get('store').findRecord('evento', params.evento_id)
+            evento: this.get('store').findAll('evento'),
+            // usuario: this.get('store').findRecord('usuario', params.id)
         });
     },
 
